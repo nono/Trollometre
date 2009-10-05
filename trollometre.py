f4dedacf629d90014bb67e5d99ba289cd2dd48a1
Bruno Michel <bmichel@menfin.info>
2009-10-05 02:21:38 +0200

    Trying to play with lxml

--- trollometre.py
+++ trollometre.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
+import lxml.html
 import os.path
 import sys
 import tornado.escape
+import tornado.httpclient
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
@@ -21,20 +23,32 @@ class Application(tornado.web.Application):
 
 
 class MainHandler(tornado.web.RequestHandler):
-  def get(self):
-    self.render("index.html")
+    def get(self):
+        self.render("index.html")
 
 
 class MeasureHandler(tornado.web.RequestHandler):
-  def get(self):
-    url = self.get_argument("url")
-    self.write(url)
+    @tornado.web.asynchronous
+    def get(self):
+        url = self.get_argument("url")
+        http = tornado.httpclient.AsyncHTTPClient()
+        http.fetch(url, callback=self.async_callback(self.on_response, url))
+
+    def on_response(self, url, response):
+        if response.error: raise tornado.web.HTTPError(500)
+        doc = lxml.html.fromstring(response.body)
+        doc.make_links_absolute(url)
+        base = lxml.html.Element('base', dict(href=url))
+        doc.head.append(base)
+        body = lxml.html.tostring(doc)
+        self.write(body)
+        self.finish()
 
 
 if __name__ == "__main__":
-  port = 8000
-  if len(sys.argv) > 1:
-    port = int(sys.argv[1])
-  http_server = tornado.httpserver.HTTPServer(Application())
-  http_server.listen(port)
-  tornado.ioloop.IOLoop.instance().start()
+    port = 8000
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+    http_server = tornado.httpserver.HTTPServer(Application())
+    http_server.listen(port)
+    tornado.ioloop.IOLoop.instance().start()
